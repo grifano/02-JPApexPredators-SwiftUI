f0b1cd2fa1765c28b93f2fc3e3625dc120f17e62
Serhii Orlenko <sorlenk@gmail.com>
2026-06-27 06:38:46 +0200

    Initial Commit

---
+++ 02-JPApexPredators/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  02-JPApexPredators
+//
+//  Created by sorlenko on 27/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 02-JPApexPredators/_2_JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2_JPApexPredatorsApp.swift
+//  02-JPApexPredators
+//
+//  Created by sorlenko on 27/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct _2_JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
