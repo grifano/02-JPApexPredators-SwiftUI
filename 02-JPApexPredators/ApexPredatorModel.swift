7d86e287113192339d262d6d55a40f5ed973c122
Serhii Orlenko <sorlenk@gmail.com>
2026-06-28 19:37:45 +0200

    Lesson 4 - List

--- 02-JPApexPredators/ApexPredatorModel.swift
+++ 02-JPApexPredators/ApexPredatorModel.swift
@@ -5,10 +5,13 @@
 //  Created by sorlenko on 27/06/2026.
 //
 
-struct ApexPredatorModel: Decodable {
+import Foundation
+import SwiftUI
+
+struct ApexPredatorModel: Decodable, Identifiable {
     let id: Int
     let name: String
-    let type: String
+    let type: APType
     let latitude: Double
     let longitude: Double
     let movies: [String]
@@ -19,5 +22,26 @@ struct ApexPredatorModel: Decodable {
         let id: Int
         let movie: String
         let sceneDescription: String
+    }
+    
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
+    
+    enum APType: String, Decodable {
+        case land
+        case sea
+        case air
+        
+        var background: Color {
+            switch self {
+            case .land:
+                .brown
+            case .sea:
+                .blue
+            case .air:
+                .teal
+            }
+        }
     }
 }
--- 02-JPApexPredators/ContentView.swift
+++ 02-JPApexPredators/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let predators = Predators()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(predators.allPredators) { predator in
+            HStack {
+                // Image
+                Image(predator.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .shadow(color: .white, radius: 4)
+                
+                VStack(alignment: .leading) {
+                    // Name
+                    Text(predator.name)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                    
+                    // Type
+                    Text(predator.type.rawValue)
+                        .font(.callout)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 8)
+                        .background(predator.type.background)
+                        .clipShape(.capsule)
+                }
+                .padding(.leading, 8)
+            }
+            .padding(.vertical, 12)
+            
         }
-        .padding()
+        .preferredColorScheme(.dark)
     }
 }
 
